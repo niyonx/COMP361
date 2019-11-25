a6dc2a763d24e905b880774aa9b871306768449e
niyonx <kenkenysy@gmail.com>
2019-11-24 23:50:03 -0500

    3/6 pass

--- COMP-361-A3-F2019-niyonx/tests/a3.py
+++ COMP-361-A3-F2019-niyonx/tests/a3.py
@@ -39,19 +39,6 @@ def python2_vs_python3():
 from numpy import arange
 
 
-def trapezoid(f, a, b, n):
-    '''
-    Integrates f between a and b using n panels (n+1 points)
-    '''
-    h = (b - a) / n
-    x = a + h * arange(n + 1)
-    I = f(x[0]) / 2
-    for i in range(1, n):
-        I += f(x[i])
-    I += f(x[n]) / 2
-    return h * I
-
-
 def problem_6_1_18(x):
     '''
     We will solve problem 6.1.18 in the textbook.
@@ -67,6 +54,18 @@ def problem_6_1_18(x):
 
     def f(t):
         return math.sin(t) / t
+
+    def trapezoid(f, a, b, n):
+        '''
+        Integrates f between a and b using n panels (n+1 points)
+        '''
+        h = (b - a) / n
+        x = a + h * arange(n + 1)
+        I = f(x[0]) / 2
+        for i in range(1, n):
+            I += f(x[i])
+        I += f(x[n]) / 2
+        return h * I
 
     return trapezoid(f, 0.000000000001, x, 100)
     raise Exception("Not implemented")
@@ -92,9 +91,29 @@ def example_6_12():
     x_data = array([1.2, 1.7, 2.0, 2.4, 2.9, 3.3])
     y_data = array([-0.36236, 0.12884, 0.41615, 0.73739, 0.97096, 0.98748])
 
-    ## YOUR CODE HERE
+    def f(x):
+        return -1 * math.cos(x)
+
+    def newton_coeffs(x_data, y_data):
+        '''
+        Returns the coefficients of the Newton polynomial
+        '''
+        a = y_data.copy()
+        m = x_data.size
+        assert (m == y_data.size)
+        for k in range(1, m):  # go through columns of the table
+            for i in range(k, m):  # go through the lines below the diagonal
+                a[i] = (a[i] - a[k - 1]) / (x_data[i] - x_data[k - 1])
+                # print(i, k, a[i])
+            # now a contains column k of the table
+        return a
+
+    return newton_coeffs(x_data, y_data)
+
     raise Exception("Not implemented")
 
+
+# print(example_6_12())
 
 '''
     Part 3: Initial-Value Problems
@@ -116,12 +135,152 @@ def problem_7_1_8(x):
     Test: function 'test_problem_7_1_8' in 'tests/test_problem_7_1_8.py'
     Hint: use Runge-Kutta 4.
     '''
-    g = 9.80665  # m/s**2
-    c = 0.2028  # kg/m
-    m = 80  # kg
 
-    ## YOUR CODE HERE
+    # g = 9.80665  # m/s**2
+    # c = 0.2028  # kg/m
+    # m = 80  # kg
+
+    def runge_kutta_4(F, x0, y0, x, h):
+        '''
+        Return y(x) given the following initial value problem:
+        y' = F(x, y)
+        y(x0) = y0 # initial conditions
+        h is the increment of x used in integration
+        F = [y'[0], y'[1], ..., y'[n-1]]
+        y = [y[0], y[1], ..., y[n-1]]
+        '''
+        X = []
+        Y = []
+        X.append(x0)
+        Y.append(y0)
+        while x0 < x:
+            k0 = F(x0, y0)
+            k1 = F(x0 + h / 2.0, y0 + h / 2.0 * k0)
+            k2 = F(x0 + h / 2.0, y0 + h / 2 * k1)
+            k3 = F(x0 + h, y0 + h * k2)
+            y0 = y0 + h / 6.0 * (k0 + 2 * k1 + 2.0 * k2 + k3)
+            x0 += h
+            X.append(x0)
+            Y.append(y0)
+        return array(X), array(Y)
+
+    g = 9.80665
+    c_D = 0.2028
+    m = 80
+
+    def F(x, y):
+        F = numpy.zeros((2,), dtype=float)
+        F[0] = y[1]
+        F[1] = - (g - (c_D / m) * y[1] ** 2)
+        return F
+
+    x0 = 0.0  # start of the integration
+    xStop = 100  # end of the integration
+    y0 = numpy.asarray([x, 0.], dtype=float)
+    h = 0.1
+    freq = 20
+
+    X, Y = runge_kutta_4(F, x0, y0, xStop, h)
+
+    y_sort_inds = numpy.argsort(Y[:, 0])
+    y_sorted = Y[y_sort_inds, 0]
+    x_sorted = X[y_sort_inds]
+
+    def swap(a, i, j):
+        if len(numpy.shape(a)) == 1:
+            a[i], a[j] = a[j], a[i]  # unpacking
+        else:
+            a[[i, j], :] = a[[j, i], :]
+
+    def gauss_substitution(a, b):
+        n, m = numpy.shape(a)
+        n2, = numpy.shape(b)
+        assert (n == n2)
+        x = numpy.zeros(n)
+        for i in range(n - 1, -1, -1):  # decreasing index
+            x[i] = (b[i] - numpy.dot(a[i, i + 1:], x[i + 1:])) / a[i, i]
+        return x
+
+    def gauss_elimination_pivot(a, b, verbose=False):
+        n, m = numpy.shape(a)
+        n2, = numpy.shape(b)
+        assert (n == n2)
+        # New in pivot version
+        s = numpy.zeros(n)
+        for i in range(n):
+            s[i] = max(abs(a[i, :]))
+        for k in range(n - 1):
+            # New in pivot version
+            p = numpy.argmax(abs(a[k:, k]) / s[k:]) + k
+            swap(a, p, k)
+            swap(b, p, k)
+            swap(s, p, k)
+            # The remainder remains as in the previous version
+            for i in range(k + 1, n):
+                assert (a[k, k] != 0)  # this shouldn't happen now, unless the matrix is singular
+                if (a[i, k] != 0):  # no need to do anything when lambda is 0
+                    lmbda = a[i, k] / a[k, k]  # lambda is a reserved keyword in Python
+                    a[i, k:n] = a[i, k:n] - lmbda * a[k, k:n]  # list slice operations
+                    b[i] = b[i] - lmbda * b[k]
+                if verbose:
+                    print(a, b)
+
+    def gauss_pivot(a, b):
+        gauss_elimination_pivot(a, b)
+        return gauss_substitution(a, b)  # as in the previous version
+
+    def polynomial_fit(x_data, y_data, m):
+        '''
+        Returns the ai
+        '''
+        # x_power[i] will contain sum_i x_i^k, k = 0, 2m
+        m += 1
+        x_powers = numpy.zeros(2 * m)
+        b = numpy.zeros(m)
+        for i in range(2 * m):
+            x_powers[i] = sum(x_data ** i)
+            if i < m:
+                b[i] = sum(y_data * x_data ** i)
+        a = numpy.zeros((m, m))
+        for k in range(m):
+            for j in range(m):
+                a[k, j] = x_powers[j + k]
+        return gauss_pivot(a, b)[::-1]
+
+    def f(x):
+        return numpy.polyval(polynomial_fit(x_sorted, y_sorted, 3), x)
+
+    def false_position(f, a, b, delta_x, max_iter=1000):
+        '''
+        f is the function for which we will find a zero
+        a and b define the bracket
+        delta_x is the desired accuracy
+        Returns ci such that |ci-c_{i-1}| < delta_x
+        '''
+        fa = f(a)
+        fb = f(b)
+        if numpy.sign(fa) == numpy.sign(fb):
+            raise Exception("Root hasn't been bracketed")
+        estimates = []
+        for i in range(max_iter):
+            c = (a * fb - b * fa) / (fb - fa)
+            estimates.append(c)
+            fc = f(c)
+            if numpy.sign(fc) == numpy.sign(fa):
+                a = c
+                fa = fc
+            else:
+                b = c
+                fb = fc
+            if len(estimates) >= 2 and abs(estimates[-1] - estimates[-2]) <= delta_x:
+                break
+        return c
+
+    return false_position(f, 0, 100, 0.001, 1000)
     raise Exception("Not implemented")
+
+
+# print(problem_7_1_8(5000))
 
 
 def problem_7_1_11(x):
